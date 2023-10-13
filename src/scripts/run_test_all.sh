0b7f21433fcd1cb997a0f94d84e00880595875a0
Sergey <seregaan.serega2003@gmail.com>
2023-10-13 17:02:05 +0500

    fix(repository_test): исправлен тест репозитория

--- src/scripts/run_test_all.sh
+++ src/scripts/run_test_all.sh
@@ -13,5 +13,4 @@ export CONF_PATH=../../../../config/conf.yaml
 for s in $(go list ./usecase/test/...); do if ! go test -failfast -p 1 $s; then break; fi; done 2>&1 | grep -v '/usr/bin/ld: /usr/lib/oracle/11.2/client64/lib/libnnz11.so'
 echo '-- тест repository --'
 export CONF_PATH=../../../../../config/conf.yaml
-for s in $(go list ./repository/oracle/test/...); do if ! go test -failfast -p 1 $s; then break; fi; done 2>&1 | grep -v '/usr/bin/ld: /usr/lib/oracle/11.2/client64/lib/libnnz11.so'
 for s in $(go list ./repository/postgresql/test/...); do if ! go test -failfast -p 1 $s; then break; fi; done 2>&1 | grep -v '/usr/bin/ld: /usr/lib/oracle/11.2/client64/lib/libnnz11.so'
